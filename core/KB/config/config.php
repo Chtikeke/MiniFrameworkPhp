5c50c750a97c1b7ff8f7c4cd7eb32cf454110a4c
Kevin Balicot <kevinbalicot@gmail.com>
2015-05-26 13:54:27 +0200

    Update config.php
    Edit doctrine annotation metadata config for use @ORM\Annotation

--- core/KB/config/config.php
+++ core/KB/config/config.php
@@ -4,7 +4,13 @@ use DI\Container;
 
 return [
     'doctrine.metadataconfig' => \DI\factory(function(container $c) {
-        return Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration([\DI\string('{kernel.src_dir}/Gbu')], true);
+        return Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration(
+            [\DI\string('{kernel.src_dir}/Gbu')],
+            true,
+            null,
+            null,
+            false
+        );
     }),
     'entity_manager' => \DI\factory(function (Container $c) {
         return \Doctrine\ORM\EntityManager::create(
